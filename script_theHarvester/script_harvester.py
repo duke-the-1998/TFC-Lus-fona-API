1619a52bb8d63d5c009326b7d8f23df722549b6e
duke-the-1998 <miguelampfagostinho@gmail.com>
2022-11-07 18:01:17 +0000

    inicio da edição do webeye

--- script_theHarvester/script_harvester.py
+++ script_theHarvester/script_harvester.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
-
 import os
 import sys
+import json
 
 limit = "500"
 filename = "results"
+tool = "theHarvester.py"
 
 #lista targets ler de um ficheiro
 #sources menos utilizadas = ["anubis", "baidu","binaryedge", "bingapi", "bufferoverun", "censys", "fullhunt", "github-code",]
@@ -14,59 +15,37 @@ listSourcesAAA = [ "bing", "certspotter", "crtsh", "dnsdumpster", "duckduckgo", 
                "qwant", "rapiddns", "rocketreach", "securityTrails", "spyse", "sublist3r", "threatcrowd", "threatminer", "trello", 
                "twitter", "urlscan", "virustotal", "yahoo", "zoomeye" ]
 
-listSources = ["bing","certspotter"]
+listSources = [ "certspotter", "dnsdumpster"]
 
+results = []
 def scan_harvester(target, limit, filename):
-    with open(target + ".json", "a") as o:
-        o.write("["+"\n")
-        
+    """
+    TODO: escrever o que a funcao faz
+    """ 
     for source in listSources:
         print("############### " + source + " ###############")
-        command = "theHarvester.py -d " + target + " -l " + limit + " -b " + source + " -f " + filename + ""
+        command = tool + " -d " + target + " -l " + limit + " -b " + source + " -f " + filename + ""
 
         try:
             print("[+] Running the theharvester:  %s" % command)
             os.system(command)
-                        
+
             with open("results.json", "r") as input:
-                with open(target + ".json", "a") as output:
-                    for line in input:
-                        string = "\"" + source + "\"" + ":" + line
-                        x = "{" + string + "},"  
-                     #   print(x)    
-                        output.write(x+"\n")
+                json_result = json.load(input)
+                results.append(json_result)        
 
         except:
             print("erros")
-        
-    
-
-def format_file(target):
-    f = open(target + ".json")
- 
-    lines = f.readlines()
-    f.close()
-
-    data = lines[len(lines)-1]
-
-    temp = list(data) 
-    temp[len(data)-2] = "\n]"
-
-    data = ''.join(temp)
-
-    lines[len(lines)-1] = data
-
-    f = open(target + ".json", "w")
-    f.writelines(lines)
-    f.close()
   
-    
 if __name__=="__main__":
     targets = open(sys.argv[1], "r").readlines()
-    
+
     for target in targets:
         scan_harvester(target, limit, filename)
-        format_file(target)
+        
+    
+    with open(target + ".json", "a") as output:  
+        json.dump(results, output, indent=4)
     
     os.remove("results.json")
     os.remove("results.xml")
